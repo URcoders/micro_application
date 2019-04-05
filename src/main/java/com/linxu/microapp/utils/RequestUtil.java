58eabd934a1875c152f4120c2c72bb0f0b1ef4a0
linux5396 <929159338@qq.com>
2019-04-05 20:46:28 +0800

    change the Httpclient, make it final.

--- src/main/java/com/linxu/microapp/utils/RequestUtil.java
+++ src/main/java/com/linxu/microapp/utils/RequestUtil.java
@@ -1,35 +1,38 @@
 package com.linxu.microapp.utils;
 
-import com.google.gson.Gson;
-import com.linxu.microapp.dtos.WxAuthMsg;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
-import javax.validation.constraints.NotNull;
 
 /**
  * @author linxu
  * @date 2019/4/5
+ * 请求工具类：
+ * 用于微信Auth请求以及其它基于Http协议的网络请求
  */
 public class RequestUtil {
     private static final String BASE_URL = "https://api.weixin.qq.com/sns/jscode2session?";
-    private static final String APP_ID = "appid=wx2861d908ee53fba2";
-    private static final String SECRET = "secret=7c29a4dbd3513d719bce2be1d2655e49";
+    private static final String APP_ID = "appid=wx2861d908ee53fba2&";
+    private static final String SECRET = "secret=7c29a4dbd3513d719bce2be1d2655e49&";
     private static final String JS_CODE = "js_code=";
-    private static final String GRANT_TYPE = "grant_type=authorization_code";
+    private static final String GRANT_TYPE = "&grant_type=authorization_code";
+    private static final RestTemplate restTemplate = new RestTemplate();
 
     /**
-     *
      * @param code from website
      * @return a string obj  which contains openid、session_key,even unionid
+     * the return value maybe a null obj, take care of it .
      */
-    public String code2Session(@NotNull String code) {
+    public static String code2Session(String code) {
+        if (EmptyUtil.isEmpty(code)) {
+            return null;
+        }
         String url = BASE_URL + APP_ID + SECRET + JS_CODE + code + GRANT_TYPE;
-        RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<String> responseEntity = restTemplate.exchange(url, HttpMethod.GET, null, String.class);
-        if (!EmptyUtil.isEmpty(responseEntity) && responseEntity.getStatusCode() == HttpStatus.OK) {
+        if (responseEntity.getStatusCode() == HttpStatus.OK) {
+            System.err.println(responseEntity.getBody());
             return responseEntity.getBody();
         }
         return null;
